6b72bb3ffe15dfaa6aef1d4714771d4ca1d98a26
Jace Browning <jacebrowning@gmail.com>
2015-02-11 00:49:39 -0500

    Add some prototype files

--- gdm/types.py
+++ gdm/types.py
@@ -8,6 +8,31 @@ import yorm
 class Dependency(yorm.extended.AttributeDictionary):
 
     """A dictionary of `git` and `ln` arguments."""
+            
+    @property
+    def path(self):
+        return os.path.join(self.root, self.to)
+        
+    
+    def update(self, root):
+        
+    def _clone(self):
+        self._call('git', 'clone', self.clone, self.to)
+        
+    def _checkout(self):
+        self._call('git', 'checkout', self.at)
+        
+    def _link(self):
+        self._call('ln', '-sf', self.path, self.link)
+        
+        
+        
+    @staticmethod
+    def _call(*args):
+        pass
+    
+
+        
 
 
 @yorm.map_attr(all=Dependency)
@@ -16,12 +41,19 @@ class DependencyList(yorm.container.List):
     """A list of dependencies."""
 
 
-@yorm.map_attr(root=yorm.standard.String)
-@yorm.map_attr(dependencies=DependencyList)
-class Configuration(yorm.extended.AttributeDictionary):
+@yorm.map_attr(directory=yorm.standard.String)
+@yorm.map_attr(repositories=DependencyList)
+@yorm.store_instances("{self.root}/{self.filename}")
+class Configuration:
 
     """A dictionary of dependency configuration options."""
 
-    def __init__(self, root='gdm_modules'):
+    def __init__(self, root, filename='gdm.yml', location='gdm_modules'):
         super().__init__()
         self.root = root
+        self.filename = filename
+        self.location = location
+        self.repositories = []
+        
+    def __iter__(self):
+        
