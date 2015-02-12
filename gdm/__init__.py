f89c5e724d474fac5db33017be5605cb218f6da9
Jace Browning <jacebrowning@gmail.com>
2015-02-12 01:02:51 -0500

    Only require Python 3.3+

--- gdm/__init__.py
+++ gdm/__init__.py
@@ -9,7 +9,7 @@ CLI = 'gdm'
 VERSION = __project__ + '-' + __version__
 DESCRIPTION = "A very basic language-agnostic dependency manager using Git."
 
-PYTHON_VERSION = 3, 4
+PYTHON_VERSION = 3, 3
 
 if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
