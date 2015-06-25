c403a42072f1c9ec9d2d20e852429b74cac0874d
Jace Browning <jacebrowning@gmail.com>
2015-06-25 15:34:08 -0400

    Fix CWD issue by returning to root after tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -11,31 +11,33 @@ from gdm.config import Config
 from .conftest import FILES, ROOT
 
 
-@pytest.mark.integration
-def test_install():
-    """Verify dependencies can be installed."""
-    os.chdir(ROOT)
-    config = Config(FILES)
-    shutil.rmtree(config.location, ignore_errors=True)
-    assert not os.path.exists(config.location)
-
-    # clean install
-    assert gdm.install(FILES)
-    assert os.path.isdir(config.location)
-    # second install
-    assert gdm.install(FILES)
-    assert 'gdm_1' in os.listdir(config.location)
-    assert 'gdm_2' in os.listdir(config.location)
-
 
 @pytest.mark.integration
-def test_uninstall():
-    """Verify dependencies can be uninstalled."""
-    os.chdir(ROOT)
-    config = Config(FILES)
+class TestInstallAndGet:
 
-    assert gdm.install(FILES)
-    assert os.path.isdir(config.location)
+    def teardown_method(self, _):
+        os.chdir(ROOT)
 
-    assert gdm.uninstall(FILES)
-    assert not os.path.isdir(config.location)
+    def test_install(self):
+        """Verify dependencies can be installed."""
+        config = Config(FILES)
+        shutil.rmtree(config.location, ignore_errors=True)
+        assert not os.path.exists(config.location)
+
+        # clean install
+        assert gdm.install(FILES)
+        assert os.path.isdir(config.location)
+        # second install
+        assert gdm.install(FILES)
+        assert 'gdm_1' in os.listdir(config.location)
+        assert 'gdm_2' in os.listdir(config.location)
+
+    def test_uninstall(self):
+        """Verify dependencies can be uninstalled."""
+        config = Config(FILES)
+
+        assert gdm.install(FILES)
+        assert os.path.isdir(config.location)
+
+        assert gdm.uninstall(FILES)
+        assert not os.path.isdir(config.location)
