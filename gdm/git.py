0409bf69dc22cfc08fe8c86e3cf4b7b80e513ebd
Jace Browning <jacebrowning@gmail.com>
2016-01-12 22:48:40 -0500

    Refactor mixins into modules

---
+++ gdm/git.py
@@ -0,0 +1,122 @@
+"""Utilities to call Git commands."""
+
+import os
+import logging
+
+from . import common
+from .shell import call
+
+
+log = logging.getLogger(__name__)
+
+
+def git(*args, **kwargs):
+    return call('git', *args, **kwargs)
+
+
+def clone(repo, path, *, cache=None):
+    """Clone a new Git repository."""
+    cache = cache or os.path.expanduser("~/.gitcache")
+
+    name = repo.split('/')[-1]
+    if name.endswith(".git"):
+        name = name[:-4]
+
+    reference = os.path.join(cache, name + ".reference")
+    if not os.path.isdir(reference):
+        git('clone', '--mirror', repo, reference)
+
+    git('clone', '--reference', reference, repo, path)
+
+
+def fetch(repo, rev=None):
+    """Fetch the latest changes from the remote repository."""
+    git('remote', 'rm', 'origin', visible=False, ignore=True)
+    git('remote', 'add', 'origin', repo)
+    args = ['fetch', '--tags', '--force', '--prune', 'origin']
+    if rev:
+        if len(rev) == 40:
+            pass  # fetch only works with a SHA if already present locally
+        elif '@' in rev:
+            pass  # fetch doesn't work with rev-parse
+        else:
+            args.append(rev)
+    git(*args)
+
+
+def changes(visible=False):
+    """Determine if there are changes in the working tree."""
+    try:
+
+        # refresh changes
+        git('update-index', '-q', '--refresh',
+            visible=False, catch=False)
+
+        # check for uncommitted changes
+        git('diff-index', '--quiet', 'HEAD',
+            visible=visible, catch=False)
+
+        # check for untracked files
+        output = git('ls-files', '--others', '--exclude-standard',
+                     visible=visible, catch=False, capture=True)
+
+    except common.CallException:
+        return True
+
+    else:
+        filenames = output.splitlines()
+        for filename in filenames:
+            log.debug("New file: %s", filename)
+        return bool(filenames)
+
+
+def update(rev, *, clean=True, fetch=False):  # pylint: disable=redefined-outer-name
+    """Update the working tree to the specified revision."""
+    hide = {'visible': False, 'ignore': True}
+
+    git('stash', **hide)
+    if clean:
+        git('clean', '--force', '-d', '-x', visible=False)
+
+    rev = _get_sha_from_rev(rev)
+    git('checkout', '--force', rev)
+    git('branch', '--set-upstream-to', 'origin/' + rev, **hide)
+
+    if fetch:
+        # if `rev` was a branch it might be tracking something older
+        git('pull', '--ff-only', '--no-rebase', **hide)
+
+
+def get_url():
+    """Get the current repository's URL."""
+    return git('config', '--get', 'remote.origin.url',
+               visible=False, capture=True)
+
+
+def get_hash(visible=False):
+    """Get the current working tree's hash."""
+    return git('rev-parse', 'HEAD', visible=visible, capture=True)
+
+
+def get_tag():
+    """Get the current working tree's tag (if on a tag)."""
+    return git('describe', '--tags', '--exact-match',
+               visible=False, ignore=True, capture=True)
+
+
+def get_branch():
+    """Get the current working tree's branch."""
+    return git('rev-parse', '--abbrev-ref', 'HEAD',
+               visible=False, capture=True)
+
+
+def _get_sha_from_rev(rev):
+    """Get a rev-parse string's hash."""
+    if '@{' in rev:  # TODO: use regex for this
+        parts = rev.split('@')
+        branch = parts[0]
+        date = parts[1].strip("{}")
+        git('checkout', '--force', branch, visible=False)
+        rev = git('rev-list', '-n', '1', '--before={!r}'.format(date),
+                  branch, visible=False, capture=True)
+    return rev
--- gdm/shell.py
+++ gdm/shell.py
@@ -23,146 +23,41 @@ def call(name, *args, ignore=False, catch=True, capture=False, visible=True):
         log.debug(msg)
 
     if name == 'cd' and len(args) == 1:
-        os.chdir(args[0])
-    else:
-        try:
-            program = Command(name)
-            if capture:
-                line = program(*args).strip()
-                log.debug(OUT_PREFIX + line)
-                return line
-            else:
-                for line in program(*args, _iter='err'):
-                    log.debug(OUT_PREFIX + line.strip())
-        except ErrorReturnCode as exc:
-            msg = "\n  IN: '{}'{}".format(os.getcwd(), exc)
-            if ignore:
-                log.debug("Ignored error from call to '%s'", name)
-            elif catch:
-                sys.exit(msg)
-            else:
-                raise common.CallException(msg)
+        return os.chdir(args[0])
 
-
-class ShellMixin:
-    """Provides classes with shell utilities."""
-
-    @staticmethod
-    def mkdir(path):
-        call('mkdir', '-p', path)
-
-    @staticmethod
-    def cd(path, visible=True):
-        call('cd', path, visible=visible)
-
-    def ln(self, source, target):
-        dirpath = os.path.dirname(target)
-        if not os.path.isdir(dirpath):
-            self.mkdir(dirpath)
-        call('ln', '-s', source, target)
-
-    @staticmethod
-    def rm(path):
-        call('rm', '-rf', path)
-
-
-class GitMixin:
-    """Provides classes with Git utilities."""
-
-    def git_clone(self, repo, path, *, cache=None):
-        """Clone a new Git repository."""
-        cache = cache or os.path.expanduser("~/.gitcache")
-
-        name = repo.split('/')[-1]
-        if name.endswith(".git"):
-            name = name[:-4]
-
-        reference = os.path.join(cache, name + ".reference")
-        if not os.path.isdir(reference):
-            self._git('clone', '--mirror', repo, reference)
-
-        self._git('clone', '--reference', reference, repo, path)
-
-    def git_fetch(self, repo, rev=None):
-        """Fetch the latest changes from the remote repository."""
-        self._git('remote', 'rm', 'origin', visible=False, ignore=True)
-        self._git('remote', 'add', 'origin', repo)
-        args = ['fetch', '--tags', '--force', '--prune', 'origin']
-        if rev:
-            if len(rev) == 40:
-                pass  # fetch only works with a SHA if already present locally
-            elif '@' in rev:
-                pass  # fetch doesn't work with rev-parse
-            else:
-                args.append(rev)
-        self._git(*args)
-
-    def git_changes(self, visible=False):
-        """Determine if there are changes in the working tree."""
-        try:
-            # refresh changes
-            self._git('update-index', '-q', '--refresh',
-                      visible=False, catch=False)
-            # check for uncommitted changes
-            self._git('diff-index', '--quiet', 'HEAD',
-                      visible=visible, catch=False)
-            # check for untracked files
-            output = self._git('ls-files', '--others', '--exclude-standard',
-                               visible=visible, catch=False, capture=True)
-        except common.CallException:
-            return True
+    try:
+        program = Command(name)
+        if capture:
+            line = program(*args).strip()
+            log.debug(OUT_PREFIX + line)
+            return line
         else:
-            filenames = output.splitlines()
-            for filename in filenames:
-                log.debug("New file: %s", filename)
-            return bool(filenames)
+            for line in program(*args, _iter='err'):
+                log.debug(OUT_PREFIX + line.strip())
+    except ErrorReturnCode as exc:
+        msg = "\n  IN: '{}'{}".format(os.getcwd(), exc)
+        if ignore:
+            log.debug("Ignored error from call to '%s'", name)
+        elif catch:
+            sys.exit(msg)
+        else:
+            raise common.CallException(msg)
 
-    def git_update(self, rev, *, clean=True, fetch=False):
-        """Update the working tree to the specified revision."""
-        hide = {'visible': False, 'ignore': True}
 
-        self._git('stash', **hide)
-        if clean:
-            self._git('clean', '--force', '-d', '-x', visible=False)
+def mkdir(path):
+    call('mkdir', '-p', path)
 
-        rev = self._git_get_sha_from_rev(rev)
-        self._git('checkout', '--force', rev)
-        self._git('branch', '--set-upstream-to', 'origin/' + rev, **hide)
 
-        if fetch:
-            # if `rev` was a branch it might be tracking something older
-            self._git('pull', '--ff-only', '--no-rebase', **hide)
+def cd(path, visible=True):
+    call('cd', path, visible=visible)
 
-    def git_get_url(self):
-        """Get the current repository's URL."""
-        return self._git('config', '--get', 'remote.origin.url',
-                         visible=False, capture=True)
 
-    def git_get_hash(self, visible=False):
-        """Get the current working tree's hash."""
-        return self._git('rev-parse', 'HEAD', visible=visible, capture=True)
+def ln(source, target):
+    dirpath = os.path.dirname(target)
+    if not os.path.isdir(dirpath):
+        mkdir(dirpath)
+    call('ln', '-s', source, target)
 
-    def git_get_tag(self):
-        """Get the current working tree's tag (if on a tag)."""
-        return self._git('describe', '--tags', '--exact-match',
-                         visible=False, ignore=True, capture=True)
 
-    def git_get_branch(self):
-        """Get the current working tree's branch."""
-        return self._git('rev-parse', '--abbrev-ref', 'HEAD',
-                         visible=False, capture=True)
-
-    def _git_get_sha_from_rev(self, rev):
-        """Get a rev-parse string's hash."""
-        if '@{' in rev:  # TODO: use regex for this
-            parts = rev.split('@')
-            branch = parts[0]
-            date = parts[1].strip("{}")
-            self._git('checkout', '--force', branch, visible=False)
-            rev = self._git('rev-list', '-n', '1', '--before={!r}'.format(date),
-                            branch, visible=False, capture=True)
-        return rev
-
-    @staticmethod
-    def _git(*args, **kwargs):
-        return call('git', *args, **kwargs)
+def rm(path):
+    call('rm', '-rf', path)
