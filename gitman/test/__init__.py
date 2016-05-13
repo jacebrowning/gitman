ab835144866b76a55784844461c8ee9abf44080c
Jace Browning <jacebrowning@gmail.com>
2016-05-13 16:27:49 -0400

    Use unix paths in test assertions

--- gitman/test/__init__.py
+++ gitman/test/__init__.py
@@ -4,6 +4,6 @@
 def assert_calls(mock_call, expected):
     """Confirm the expected list of calls matches the mock call."""
     __tracebackhide__ = True  # pylint: disable=unused-variable
-    actual = [' '.join((str(arg) for arg in args[0]))
+    actual = [' '.join((str(arg).replace('\\', '/') for arg in args[0]))
               for args in mock_call.call_args_list]
     assert expected == actual
