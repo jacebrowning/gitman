003a7e5c8a5c20ff2d1efaa9266588a6261c437a
Daniel Brosche <Daniel.Brosche@lenze.com>
2016-11-22 13:02:57 +0100

    Added package self declaration to improve debugging support

--- gitman/__main__.py
+++ gitman/__main__.py
@@ -1,5 +1,13 @@
 """Package entry point."""
 
+# Declare itself as package if needed
+if __name__ == '__main__' and __package__ is None:
+    import os, sys, importlib
+    parent_dir = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(parent_dir))
+    __package__ = os.path.basename(parent_dir)
+    importlib.import_module(__package__)
+
 from gitman.cli import main
 
 
