24017fbb2ae146c10c42fb9c2eab83d8f68ffb50
Jace Browning <jacebrowning@gmail.com>
2016-11-24 13:16:50 -0500

    Disable import checks in __main__

--- gitman/__main__.py
+++ gitman/__main__.py
@@ -1,15 +1,17 @@
 """Package entry point."""
 
-# Declare itself as package if needed
-if __name__ == '__main__' and __package__ is None:
+# Declare itself as package if needed for better debugging support
+# pylint: disable=multiple-imports,wrong-import-position,redefined-builtin
+if __name__ == '__main__' and __package__ is None:  # pragma: no cover
     import os, sys, importlib
     parent_dir = os.path.abspath(os.path.dirname(__file__))
     sys.path.append(os.path.dirname(parent_dir))
     __package__ = os.path.basename(parent_dir)
     importlib.import_module(__package__)
 
+
 from gitman.cli import main
 
 
-if __name__ == '__main__':  # pragma: no cover (manual test)
+if __name__ == '__main__':  # pragma: no cover
     main()
