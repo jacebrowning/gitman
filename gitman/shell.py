9cbb0a17bb410e7b0e40aea57b425feb52834d93
nomorgan <nomorgan@gmail.com>
2017-01-07 02:40:22 +0100

    CI fix guidelines + linux : fix test_shell)

--- gitman/shell.py
+++ gitman/shell.py
@@ -55,11 +55,10 @@ def call(name, *args, _show=True, _ignore=False, _shell=False):
 
 
 def mkdir(path):
-    if not os.path.exists(path):
-        if os.name == 'nt':
-            call('mkdir', path, _shell=True)
-        else:
-            call('mkdir', '-p', path)
+    if os.name == 'nt':
+        call('mkdir', path, _shell=True)
+    else:
+        call('mkdir', '-p', path)
 
 
 def cd(path, _show=True):
@@ -82,16 +81,15 @@ def ln(source, target):
 
 
 def rm(path):
-    if os.path.exists(path):
-        if os.name == 'nt' :
-            if os.path.isdir(path):
-                call('rmdir', '/Q', '/S', path, 
+    if os.name == 'nt':
+        if os.path.isdir(path):
+            call('rmdir', '/Q', '/S', path,
                     _shell=True
                 )
-            else:
-                call('del', '/Q', '/F', path, _shell=True)
         else:
-            call('rm', '-rf', path)
+            call('del', '/Q', '/F', path, _shell=True)
+    else:
+        call('rm', '-rf', path)
 
 
 def show(name, *args, stdout=True):
