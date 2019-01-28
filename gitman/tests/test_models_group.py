1b1263359327d72e7fad7c2e9cd19edda2df34ed
Jace Browning <jacebrowning@gmail.com>
2019-01-28 10:15:51 -0500

    Add scaffolding for group unit tests

---
+++ gitman/tests/test_models_group.py
@@ -0,0 +1,14 @@
+# pylint: disable=redefined-outer-name,expression-not-assigned
+
+import pytest
+
+from gitman.models import Group
+
+
+@pytest.fixture
+def group():
+    return Group('foo', ['bar', 'qux'])
+
+
+def test_str_contains_name(group, expect):
+    expect(str(group)) == "['foo']"
