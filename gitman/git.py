0e3f8d0b9647212148491954eaa3061fff546dea
Faris Hamdi <fhamdi@theaiinstitute.com>
2026-03-18 17:17:55 -0400

    linter fixes and function call correction

--- gitman/git.py
+++ gitman/git.py
@@ -74,7 +74,13 @@ def clone(
                 fd.write("%s/objects" % sparse_paths_repo)
 
         git("-C", normpath, "sparse-checkout", "init", "--cone")
-        git("-C", normpath, "sparse-checkout", "set", *sanitize_sparse_paths(sparse_paths))
+        git(
+            "-C",
+            normpath,
+            "sparse-checkout",
+            "set",
+            *sanitize_sparse_paths(sparse_paths)
+        )
         git("-C", normpath, "fetch", "origin")
         git("-C", normpath, "checkout", rev)
     elif settings.CACHE_DISABLE:
@@ -252,7 +258,7 @@ def am(patch, _skip=False):
 
 def apply_sparse_checkout(sparse_paths):
     """Re-apply sparse-checkout paths to an existing working tree."""
-    git("sparse-checkout", "set", *_sanitize_sparse_paths(sparse_paths))
+    git("sparse-checkout", "set", *sanitize_sparse_paths(sparse_paths))
 
 
 def update(
